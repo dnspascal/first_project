614d20d29654197293e120babf671c74c2d0ca6c
dnspascal <102286019+dnspascal@users.noreply.github.com>
2022-07-22 15:26:14 +0300

    :construction :chore  changes on scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -8,15 +8,41 @@ class MyApp extends StatelessWidget {
   Widget build (BuildContext context){
     return MaterialApp(
       title: 'Demo',
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
       body:ListView(children: [
-ListTile(leading: Text('denis'),
+ListTile(
+  leading:CircleAvatar(),
+title: Text("denis"),
                      subtitle: Text('message'),
-                     trailing: Text('1')
-                     
-                     
-        
-      )
+                     trailing: Text('12:20'),
+                     tileColor: Color.fromARGB(255, 3, 83, 5),
+                    ),
+      
+      
+      ListTile(
+  leading:CircleAvatar( ),
+title: Text("francis"),
+                     subtitle: Text('message'),
+                     trailing: Text('13:20'),
+                     tileColor: Color.fromARGB(255, 3, 83, 5),
+                    ),
+                    ListTile(
+  leading:CircleAvatar( ),
+title: Text("mgaya"),
+                     subtitle: Text('message'),
+                     trailing: Text('15:20') ,
+                     tileColor: Color.fromARGB(255, 3, 83, 5),
+                    ),
+                    ListTile(
+  leading:CircleAvatar( ),
+title: Text("pascal"),
+                     subtitle: Text('message'),
+                     trailing: Text('12:20'),
+                     tileColor: Color.fromARGB(255, 3, 83, 5),
+                    )
+      
+      
       ],)
       
       )
