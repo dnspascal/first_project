1f55d73c607deef40bd7b1cd173fb023cfd10eac
dnspascal <102286019+dnspascal@users.noreply.github.com>
2022-07-25 12:13:02 +0300

    picture was added

--- lib/main.dart
+++ lib/main.dart
@@ -5,51 +5,44 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  Widget build (BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Demo',
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-      body:ListView(children: [
-ListTile(
-  leading:CircleAvatar(),
-title: Text("denise"),
-                     subtitle: Text('message'),
-                     trailing: Text('12:20'),
-                     tileColor: Color.fromARGB(255, 3, 83, 5),
-                    ),
-      
-      
-      ListTile(
-  leading:CircleAvatar( ),
-title: Text("francis"),
-                     subtitle: Text('message'),
-                     trailing: Text('13:20'),
-                     tileColor: Color.fromARGB(255, 3, 83, 5),
-                    ),
-                    ListTile(
-  leading:CircleAvatar( ),
-title: Text("mgaya"),
-                     subtitle: Text('message'),
-                     trailing: Text('15:20') ,
-                     tileColor: Color.fromARGB(255, 3, 83, 5),
-                    ),
-                    ListTile(
-  leading:CircleAvatar( ),
-title: Text("pascal"),
-                     subtitle: Text('message'),
-                     trailing: Text('12:20'),
-                     tileColor: Color.fromARGB(255, 3, 83, 5),
-                    )
-      
-      
-      ],)
-      
-      )
-      );
-      
-      
-    
+        title: 'Demo',
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+            body: ListView(
+          children:  [
+            ListTile(
+              leading: CircleAvatar(child: Image.asset("asset/friend.jpg", fit: BoxFit.cover,)),
+              title: Text("denise"),
+              subtitle: Text('message'),
+              trailing: Text('12:20'),
+              tileColor: Color(0xFF075E54),
+            
+            ),
+            ListTile(
+              leading: CircleAvatar(),
+              title: Text("francis"),
+              subtitle: Text('message'),
+              trailing: Text('13:20'),
+              tileColor: Color(0xFF075E54),
+            
+            ),
+            ListTile(
+              leading: CircleAvatar(),
+              title: Text("mgaya"),
+              subtitle: Text('message'),
+              trailing: Text('15:20'),
+              tileColor: Color(0xFF075E54),
+            ),
+            ListTile(
+              leading: CircleAvatar(),
+              title: Text("pascal"),
+              subtitle: Text('message'),
+              trailing: Text('12:20'),
+              tileColor: Color(0xFF075E54),
+            )
+          ],
+        )));
   }
 }
-
