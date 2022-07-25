516c5faae1f23a26d29e53ed213a2e0b49574199
dnspascal <102286019+dnspascal@users.noreply.github.com>
2022-07-25 15:32:49 +0300

    I have changed the  background image and dps

--- lib/main.dart
+++ lib/main.dart
@@ -10,39 +10,43 @@ class MyApp extends StatelessWidget {
         title: 'Demo',
         debugShowCheckedModeBanner: false,
         home: Scaffold(
-            body: ListView(
-          children:  [
-            ListTile(
-              leading: CircleAvatar(child: Image.asset("asset/friend.jpg", fit: BoxFit.cover,)),
-              title: Text("denise"),
-              subtitle: Text('message'),
-              trailing: Text('12:20'),
-              tileColor: Color(0xFF075E54),
-            
-            ),
-            ListTile(
-              leading: CircleAvatar(),
-              title: Text("francis"),
-              subtitle: Text('message'),
-              trailing: Text('13:20'),
-              tileColor: Color(0xFF075E54),
-            
-            ),
-            ListTile(
-              leading: CircleAvatar(),
-              title: Text("mgaya"),
-              subtitle: Text('message'),
-              trailing: Text('15:20'),
-              tileColor: Color(0xFF075E54),
-            ),
-            ListTile(
-              leading: CircleAvatar(),
-              title: Text("pascal"),
-              subtitle: Text('message'),
-              trailing: Text('12:20'),
-              tileColor: Color(0xFF075E54),
-            )
-          ],
+          appBar: AppBar(backgroundColor: Colors.greenAccent.withOpacity(0.5)),
+            body: Container(
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage("asset/friend.jpg"), fit: BoxFit.cover)),
+          child: ListView(
+            children: [
+              ListTile(
+                leading: CircleAvatar(backgroundImage: AssetImage("asset/Nyalandu.jpeg"),),
+                title: Text("denis"),
+                subtitle: Text('message'),
+                trailing: Text('12:20'),
+                tileColor: Color(0xFF075E54),
+              ),
+              ListTile(
+                leading: CircleAvatar(backgroundImage: AssetImage("asset/Nyalandu.jpeg")),
+                title: Text("francis"),
+                subtitle: Text('message'),
+                trailing: Text('13:20'),
+                tileColor: Color(0xFF075E54),
+              ),
+              ListTile(
+                leading: CircleAvatar(backgroundImage: AssetImage("asset/Nyalandu.jpeg")),
+                title: Text("mgaya"),
+                subtitle: Text('message'),
+                trailing: Text('15:20'),
+                tileColor: Color(0xFF075E54),
+              ),
+              ListTile(
+                leading: CircleAvatar(backgroundImage: AssetImage("asset/Nyalandu.jpeg")),
+                title: Text("pascal"),
+                subtitle: Text('message'),
+                trailing: Text('12:20'),
+                tileColor: Color(0xFF075E54),
+              )
+            ],
+          ),
         )));
   }
 }
