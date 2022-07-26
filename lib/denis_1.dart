a9ca36e7d33a2fe53bb1535782d58a65ddcff4f3
dnspascal <102286019+dnspascal@users.noreply.github.com>
2022-07-26 19:50:48 +0300

    The entire project has been changed and the new dart file is added

---
+++ lib/denis_1.dart
@@ -0,0 +1,8 @@
+class Me {
+  String title ;
+  String subTitle ;
+  String time ;
+  String picha ;
+
+  Me({ required this.title,required this.subTitle, required this.time, required this.picha});
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:first_project/denis_1.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -5,48 +6,64 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  final List<Me> listOfCharts = [
+    Me(
+        title: "Denis",
+        subTitle: "English",
+        time: "00:00",
+        picha: "asset/Nyalandu.jpeg"),
+    Me(
+        title: " Hossam",
+        subTitle: "fresh mzee",
+        time: "00:05",
+        picha: "asset/Nyalandu.jpeg"),
+    Me(
+        title: " Goodluck",
+        subTitle: " Mechi ya Barcelona leo",
+        time: "00:08",
+        picha: "asset/Nyalandu.jpeg"),
+    Me(
+        title: "Edgar",
+        subTitle: " kama kawa mzee",
+        time: "00:10",
+        picha: "friend2.jpg"),
+    Me(
+        title: " Fred",
+        subTitle: "Tutaipangilia",
+        time: "00:20",
+        picha: "asset/Nyalandu.jpeg"),
+    Me(
+        title: "Jacob",
+        subTitle: "Leo class siendi",
+        time: "00:30",
+        picha: "asset/Nyalandu.jpeg"),
+    Me(
+        title: "Peter",
+        subTitle: "Nilisha mwambia ",
+        time: "00:40",
+        picha: "asset/Nyalandu.jpeg"),
+    Me(
+        title: "James",
+        subTitle: "Sema tutaona fresh",
+        time: "00:50",
+        picha: "friend.jpg"),
+  ];
   Widget build(BuildContext context) {
     return MaterialApp(
         title: 'Demo',
         debugShowCheckedModeBanner: false,
         home: Scaffold(
-          appBar: AppBar(backgroundColor: Colors.greenAccent.withOpacity(0.5)),
-            body: Container(
-          decoration: BoxDecoration(
-              image: DecorationImage(
-                  image: AssetImage("asset/friend.jpg"), fit: BoxFit.cover)),
-          child: ListView(
-            children: [
-              ListTile(
-                leading: CircleAvatar(backgroundImage: AssetImage("asset/Nyalandu.jpeg"),),
-                title: Text("denis"),
-                subtitle: Text('message'),
-                trailing: Text('12:20'),
-                tileColor: Color(0xFF075E54),
-              ),
-              ListTile(
-                leading: CircleAvatar(backgroundImage: AssetImage("asset/Nyalandu.jpeg")),
-                title: Text("francis"),
-                subtitle: Text('message'),
-                trailing: Text('13:20'),
-                tileColor: Color(0xFF075E54),
-              ),
-              ListTile(
-                leading: CircleAvatar(backgroundImage: AssetImage("asset/Nyalandu.jpeg")),
-                title: Text("mgaya"),
-                subtitle: Text('message'),
-                trailing: Text('15:20'),
-                tileColor: Color(0xFF075E54),
-              ),
-              ListTile(
-                leading: CircleAvatar(backgroundImage: AssetImage("asset/Nyalandu.jpeg")),
-                title: Text("pascal"),
-                subtitle: Text('message'),
-                trailing: Text('12:20'),
-                tileColor: Color(0xFF075E54),
-              )
-            ],
-          ),
-        )));
+          appBar: AppBar(backgroundColor: Color.fromARGB(255, 2, 51, 3)),
+          body: ListView(children: [
+            ...listOfCharts.map((value) {
+              return ListTile(
+                leading: CircleAvatar(backgroundImage: AssetImage(value.picha)),
+                title: Text(value.title),
+                subtitle: Text(value.subTitle),
+                trailing: Text(value.time),
+              );
+            }).toList(),
+          ]),
+        ));
   }
 }
